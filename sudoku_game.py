bd12fbcf28a0cf6e39907cb84ad506842bf3d513
yovany sora olaya <yovannysora@gmail.com>
2025-04-24 11:48:42 -0500

    Creacion de la funcion de  validacion

--- sudoku_game.py
+++ sudoku_game.py
@@ -3,3 +3,6 @@ class SudokuGame:
     def __init__(self, display_type='numerico', difficulty='facil'):
         self.board = []
         self.solution = []
+    def is_valid(self, row, col, num):
+        # Verifica si un número es válido en la celda
+        pass
