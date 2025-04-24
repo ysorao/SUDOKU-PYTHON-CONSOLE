f51058e849c9d45a465030ad232492f6b1a75cf8
yovany sora olaya <yovannysora@gmail.com>
2025-04-24 11:47:53 -0500

    Creación de la clase principal

--- sudoku_game.py
+++ sudoku_game.py
@@ -1,1 +1,5 @@
 # MODULO PARA LA LOGICA DEL JUEGO SUDOKU - Asignado a Desarrollador Yovany Sora Olaya
+class SudokuGame:
+    def __init__(self, display_type='numerico', difficulty='facil'):
+        self.board = []
+        self.solution = []
