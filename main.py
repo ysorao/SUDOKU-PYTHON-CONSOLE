85fc84ddad25a06887322596b55447e854059355
Katherine <ltroche96826@universidadean.edu.co>
2025-04-24 20:52:02 -0500

    Desarrollo del módulo principal

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # MODULO PRINCIPAL - Asignado a Desarrollador Katherine Trochez
 
+def main():
+    print("=== SUDOKU 4x4 ===")
+    print("1. Jugar")
+    print("2. Ver mejores puntajes")
+    print("3. Salir")
+
+if __name__ == "__main__":
+    main()
+
