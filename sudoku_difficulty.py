8e526c1c6ec4b5f91ded0da7f6e26d09c3d7c69e
Astromelia-1 <flora48721@gmail.com>
2025-04-25 12:35:06 -0500

    Creación de la clase difucultad

--- sudoku_difficulty.py
+++ sudoku_difficulty.py
@@ -1,2 +1,10 @@
 # MODULO PARA LA DIFICULTAD DEL JUEGO - Asignado a Desarrollador Flor Alba Tovar
+class SudokuDifficulty:
+    def __init__(self, difficulty='facil'):
+        self.difficulty = difficulty
 
+    def get_difficulty_options(self):
+        return {
+            '1': 'facil',
+            '2': 'dificil'
+        }
