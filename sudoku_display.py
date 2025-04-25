5a36f24de039fa172f468b5d2880ffa66aab461a
Geraldyn Palacio <gpalaci80807@universidadean.edu.co>
2025-04-25 08:59:50 -0500

    crear clase SudokuDisplay con display_type y símbolos

--- sudoku_display.py
+++ sudoku_display.py
@@ -3,23 +3,3 @@ class SudokuDisplay:
     def __init__(self, display_type='numerico'):
         self.display_type = display_type
         self.symbols = ['♣', '♦', '♥', '♠']
-
-    def convert_to_display(self, num):
-        # Convierte número a su representación visual
-        pass
-
-    def convert_from_display(self, value):
-        # Convierte visualización a número
-        pass
-
-    def get_display_options(self):
-        # Opciones de visualización disponibles
-        return {
-            '1': 'numerico',
-            '2': 'letras',
-            '3': 'simbolos'
-        }
-    
-    def print_display_menu(self):
-        # Menú visual para seleccionar tipo de display
-        pass
