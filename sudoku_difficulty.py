e24416ae012232d580f42eb14faceef39b024ae1
Astromelia-1 <flora48721@gmail.com>
2025-05-02 12:01:10 -0500

    Parte final módulo dificultad

--- sudoku_difficulty.py
+++ sudoku_difficulty.py
@@ -1,22 +1,23 @@
 # MODULO PARA LA DIFICULTAD DEL JUEGO - Asignado a Desarrollador Flor Alba Tovar
+
 class SudokuDifficulty:
     def __init__(self, difficulty='facil'):
         self.difficulty = difficulty
+        self.cells_to_remove = 6 if difficulty == 'facil' else 10
 
     def get_difficulty_options(self):
         return {
             '1': 'facil',
             '2': 'dificil'
         }
-    
+
     def print_difficulty_menu(self):
-        # Muestra el menú de dificultad
-        pass
+        print("\nSelecciona el nivel de dificultad:")
+        print("1. Fácil (6 casillas vacías)")
+        print("2. Difícil (10 casillas vacías)")
 
     def get_border(self):
-        # Devuelve el tipo de borde del tablero
-        pass
-    
+        return '*' if self.difficulty == 'facil' else '-'
+
     def get_cells_to_remove(self):
-        # Devuelve cantidad de casillas vacías según dificultad
-        Pass
+        return self.cells_to_remove 
